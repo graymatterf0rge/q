f1a573aeadc35a3befd4be7e9e1950b6d6033392
ub3rb3457 <31402226+ub3rb3457@users.noreply.github.com>
2022-04-08 05:41:11 -0400

    Add local changes to new git branch

--- bin/commitToNewBranch.sh
+++ bin/commitToNewBranch.sh
@@ -2,14 +2,12 @@
 
 read -p 'New Branch Name:' newBranch
 
-git switch -b $newBranch
+git switch -c $newBranch
 
 git add .
 
 git commit -m "Add local changes to new git branch"
 
-git switch main
+git switch -
 
 git clean -f
-
-git switch $newBranch
