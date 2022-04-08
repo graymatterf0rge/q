9f9f2d4eb17c61a46b57451ec1002ae91f92b91d
ub3rb3457 <31402226+ub3rb3457@users.noreply.github.com>
2022-04-08 05:38:44 -0400

    Add local changes to new git branch

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,30 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/vue3"
-}
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  "framework": "@storybook/vue3",
+  "features": {
+    "interactionsDebugger": true
+  },
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+
+  /**
+   * Extend Vite config
+   * @param {import('vite').UserConfig} config
+   * @param {'DEVELOPMENT'|'PRODUCTION'} [configType]
+   * @returns {Promise<*>}
+   */
+  async viteFinal(config, {
+    configType
+  }) {
+    // Needed only for development mode: `npm run storybook`
+    config.optimizeDeps = configType === 'PRODUCTION' ? config.optimizeDeps : { ...(config.optimizeDeps || {}),
+      include: [...(config?.optimizeDeps?.include || []), // Fix: `@storybook/addon-interactions` exports is not defined or `jest-mock` does not provide an export named 'fn'
+      'jest-mock']
+    }; // ...
+
+    return config;
+  }
+
+};
